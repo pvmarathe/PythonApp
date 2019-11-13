4c896b8602a8eced85965ccabceaac664cbff4ea
pvmarathe <prasadvmarathe@gmail.com>
2019-11-12 21:09:35 -0500

    added code for class distributions

--- DescriptiveStatistics.py
+++ DescriptiveStatistics.py
@@ -4,7 +4,7 @@ from pandas import set_option
 filename = "pima-indians-diabetes.data.csv"
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 data = read_csv(filename, names=names)
-set_option('display.width', 125)
+set_option('display.width', 100)
 set_option('precision', 3)
 description = data.describe()
 print(description)
