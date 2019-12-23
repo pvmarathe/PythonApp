7df1fd7d705e954ac1edd955abc8be39dfe6ecfa
pvmarathe <prasadvmarathe@gmail.com>
2019-12-22 20:05:13 -0500

    Git cheetsheet

---
+++ GitBash_Commands.py
@@ -0,0 +1,15 @@
+#Question on finding where Python is installed on windows OS
+print("Q1: How to check where you have installed Python on windows?")
+print("A1: Type cmd in start and type 'python' in command prompt, then type 'import sys', then type 'sys.executable' and you will get the python path ")
+
+#pwd gitbash command
+print("Q2: What does pwd stand for?")
+print("A2: present working directory")
+
+#ls gitbash command
+print("Q3: What does ls stand for?")
+print("A3: ls stands for list and it lists all the files in the path or the default path")
+
+#dc gitbash command
+print("Q4: What does cd stand for?")
+print("A4: cd stands for change directory")
